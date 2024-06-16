a3c05b7a1ae7e308c4964af28e0f190b17950e78
wj-jason <jason.wang1709@gmail.com>
2024-06-16 16:22:42 -0400

    bug fixes

--- main.py
+++ main.py
@@ -7,3 +7,8 @@ from tensorflow.keras.losses import BinaryCrossentropy
 from tensorflow.keras.callbacks import EarlyStopping
 from sklearn.model_selection import train_test_split
 
+from hierarchical_classification.resnet import Resnet50
+
+test = Resnet50(num_classes=2)
+
+print("here")
